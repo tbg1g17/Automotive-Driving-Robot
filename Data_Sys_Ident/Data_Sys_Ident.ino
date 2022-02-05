37fb6f5e5bcbb9895d07835484c531c633a1b1b1
Mahmoud G <mg8g17@soton.ac.uk>
2022-02-05 16:07:47 +0000

    Added Arduino sketch 4 collecting open-loop input/output

---
+++ Data_Sys_Ident/Data_Sys_Ident.ino
@@ -0,0 +1,22 @@
+/* (04/02/2022)
+ * 
+ * This code is to collect data from inputs and outputs of our robot and store the data in a csv file.
+ * 
+ * The code is to be used with the open-loop code created by Tom and the code for creating random PWM signal
+ * where open-loop operation of the entire robot is held so that inputs and outputs to the motor are 
+ * recorded and used with MATLAB's system-identification toolbox, hence finding the (approx.) TF of our 
+ * entire system, relating the input voltage to the pedal's position (i.e., system output), or relating motor 
+ * position to pedal position.
+ * 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
